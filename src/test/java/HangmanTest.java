89808cb527eb7dd7f5d57390165d1170a93c1b12
ekmust2499 <katiammm0204@mail.ru>
2018-11-15 22:37:41 +0500

    .

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,4 +1,4 @@
-package main.java; 
+package test.java; 
 
 import static org.junit.jupiter.api.Assertions.*; 
 import org.junit.jupiter.api.Test;
--- src/test/java/TruthOrDareTest.java
+++ src/test/java/TruthOrDareTest.java
@@ -1,4 +1,4 @@
-package main.java;
+package test.java;
  
 import static org.junit.jupiter.api.Assertions.*;
 
